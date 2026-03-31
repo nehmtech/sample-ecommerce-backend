8fa5a9ddaeb186bf16b88dc2343bf8c2e0224ff9
nehmtech <nehmtech.ke@gmail.com>
2026-03-31 20:40:59 +0300

    updated

--- project/settings.py
+++ project/settings.py
@@ -28,11 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['sample-ecommerce-backend-production.up.railway.app']
+ALLOWED_HOSTS = ['sample-ecommerce-backend-production.up.railway.app', '*.railway.app']
 
-SITE_URL = 'http://127.0.0.1:8000'
+SITE_URL = 'sample-ecommerce-backend-production.up.railway.app'
 FRONTEND_URL = 'https://sample-ecommerce-frontend-production.up.railway.app'
 
 
